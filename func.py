9a51a03aad1dcd6ba43bb9fb619691fcce331a7c
katiaMOC <katia.ordo.can1998@gmail.com>
2024-02-14 03:30:49 -0500

    first commit

---
+++ func.py
@@ -0,0 +1,210 @@
+import pandas as pd
+import numpy as np
+
+def analizar_valores_sd(dataframe):
+    """
+    Analiza la presencia de valores 'SD' en cada columna del DataFrame.
+
+    Parameters:
+    dataframe (pd.DataFrame): El DataFrame a analizar.
+
+    Returns:
+    pd.DataFrame: Un DataFrame que muestra la cantidad y porcentaje de valores 'SD' en cada columna.
+    """
+    columnas_con_sd = dataframe.columns
+    resultados = []
+
+    for columna in columnas_con_sd:
+        cantidad_sd = dataframe[columna].eq('SD').sum()
+        porcentaje_sd = (cantidad_sd / len(dataframe)) * 100
+        resultados.append({'Columna': columna, 'Cantidad de SD': cantidad_sd, 'Porcentaje de SD': porcentaje_sd})
+
+    resultados_df = pd.DataFrame(resultados)
+    resultados_con_sd = resultados_df[resultados_df['Cantidad de SD'] > 0]
+
+    return resultados_con_sd
+
+def data_cleaning(df, drop_duplicates=False, drop_na=False, fill_na=None, convert_to_datetime=None, uppercase_columns=None,
+                  lowercase_columns=None, titlecase_columns=None, strip_spaces=True, rename_columns=None, drop_columns=None,
+                  categorize_columns=None, replace_values=None, new_columns=None, convert_date_columns=None, 
+                  convert_to_int_columns=None, convert_to_float=None, new_columns2=None):
+    """
+    Realiza el proceso de limpieza de datos en un DataFrame.
+
+    Parámetros:
+    - df (pd.DataFrame): El DataFrame que se va a limpiar.
+    
+    - drop_duplicates (bool): Elimina duplicados si es True.
+      Ejemplo: cleaned_df = data_cleaning(df_tu_data_frame, drop_duplicates=True)
+    
+    - drop_na (bool): Elimina filas con valores nulos si es True.
+      Ejemplo: cleaned_df = data_cleaning(df_tu_data_frame, drop_na=True)
+
+    - fill_na (dict): Un diccionario donde las claves son los nombres de columnas y los valores son valores para rellenar los nulos.
+      Ejemplo: fill_na_dict = {'gravedad': 'leve'}
+               cleaned_df = data_cleaning(df_tu_data_frame, fill_na=fill_na_dict)
+        
+    - convert_to_datetime (list): Lista de columnas para convertir a tipo de dato datetime.
+      Ejemplo: columns_to_convert = ['fecha', 'hora']
+               cleaned_df = data_cleaning(df_tu_data_frame, convert_to_datetime=columns_to_convert)
+
+    - uppercase_columns (list): Lista de columnas para convertir a mayúsculas.
+      Ejemplo: columns_to_uppercase = ['nombre', 'apellido']
+               cleaned_df = data_cleaning(df_tu_data_frame, uppercase_columns=columns_to_uppercase)
+
+    - lowercase_columns (list): Lista de columnas para convertir a minúsculas.
+      Ejemplo: columns_to_lowercase = ['Ciudad', 'Pais']
+               cleaned_df = data_cleaning(df_tu_data_frame, lowercase_columns=columns_to_lowercase)
+
+    - titlecase_columns (list): Lista de columnas para convertir a formato de título (primera letra en mayúscula, resto en minúscula).
+      Ejemplo: columns_to_titlecase = ['titulo', 'categoria']
+               cleaned_df = data_cleaning(df_tu_data_frame, titlecase_columns=columns_to_titlecase)
+            
+    - strip_spaces (bool): Elimina espacios en blanco alrededor de los valores de las celdas si es True.
+      Ejemplo: cleaned_df = data_cleaning(df_tu_data_frame, strip_spaces=True)
+
+    - rename_columns (dict): Un diccionario donde las claves son los nombres de las columnas actuales y los valores son los nuevos nombres.
+      Ejemplo: rename_dict = {'Vieja_Columna': 'Nueva_Columna'}
+               cleaned_df = data_cleaning(df_tu_data_frame, rename_columns=rename_dict)
+    
+    - drop_columns (list): Lista de columnas para eliminar.
+      Ejemplo: columns_to_drop = ['columna1', 'columna2']
+               cleaned_df = data_cleaning(df_tu_data_frame, drop_columns=columns_to_drop)
+        
+    - categorize_columns (list): Lista de columnas para convertir a tipo de dato categoría.
+      Ejemplo: columns_to_categorize = ['categoria1', 'categoria2']
+               cleaned_df = data_cleaning(df_tu_data_frame, categorize_columns=columns_to_categorize)
+        
+    - replace_values (dict): Un diccionario donde las claves son los nombres de las columnas y los valores son diccionarios de reemplazo.
+      Ejemplo: replace_dict = {'columna1': {'Antiguo1': 'Nuevo1', 'Antiguo2': 'Nuevo2'}}
+               cleaned_df = data_cleaning(df_tu_data_frame, replace_values=replace_dict)
+
+    - new_columns (dict): Un diccionario donde las claves son los nombres de las nuevas columnas y los valores son valores para esas columnas.
+      Ejemplo: new_columns_dict = {'nueva_columna': 0}
+               cleaned_df = data_cleaning(df_tu_data_frame, new_columns=new_columns_dict)
+               
+    - new_columns2 (dict): Un diccionario donde las claves son los nombres de las nuevas columnas y los valores son expresiones
+                          para calcular el contenido de las nuevas columnas basadas en otras columnas existentes. 
+      Ejemplo: {'nueva_columna1': 'columna_existente * 2'}
+      cleaned_df = data_cleaning(df_tu_data_frame, new_columns2=new_columns_dict)
+
+ 
+    - convert_date_columns (dict): Un diccionario donde las claves son los nombres de las columnas y los valores son los formatos de fecha.
+      Ejemplo: date_columns_dict = {'fecha': '%Y-%m-%d', 'hora': '%H:%M:%S'}
+               cleaned_df = data_cleaning(df_tu_data_frame, convert_date_columns=date_columns_dict)
+
+    - convert_to_int_columns (list): Lista de columnas para convertir a tipo de dato entero.
+      Ejemplo: columns_to_int = ['columna1', 'columna2']
+               cleaned_df = data_cleaning(df_tu_data_frame, convert_to_int_columns=columns_to_int)
+    
+    - convert_to_float (list): Lista de columnas para convertir a tipo de dato float.
+      Ejemplo: columns_to_float = ['columna1', 'columna2']
+               cleaned_df = data_cleaning(df_tu_data_frame, convert_to_float=columns_to_float)          
+            
+    Retorna:
+    pd.DataFrame: El DataFrame limpio.
+    """
+
+    cleaned_df = df.copy()
+
+    # Eliminar duplicados
+    if drop_duplicates:
+        cleaned_df.drop_duplicates(inplace=True)
+        
+
+    # Eliminar filas con valores nulos
+    if drop_na:
+        cleaned_df.dropna(inplace=True)
+        
+
+    # Rellenar valores nulos
+    if fill_na:
+        cleaned_df.fillna(fill_na, inplace=True)
+
+        
+    # Convertir columnas a tipo datetime
+    if convert_to_datetime:
+        for column in convert_to_datetime:
+            cleaned_df[column] = pd.to_datetime(cleaned_df[column], errors='coerce')
+            
+
+    # Convertir columnas a mayúsculas
+    if uppercase_columns:
+        for column in uppercase_columns:
+            cleaned_df[column] = cleaned_df[column].str.upper()
+            
+
+    # Convertir columnas a minúsculas
+    if lowercase_columns:
+        for column in lowercase_columns:
+            cleaned_df[column] = cleaned_df[column].str.lower()
+        
+
+    # Convertir columnas a formato de título
+    if titlecase_columns:
+        for column in titlecase_columns:
+            cleaned_df[column] = cleaned_df[column].str.title()
+            
+            
+    # Tratar columnas con espacios
+    if strip_spaces:
+        cleaned_df = cleaned_df.applymap(lambda x: x.strip() if isinstance(x, str) else x)
+        
+
+    # Renombrar columnas
+    if rename_columns:
+        cleaned_df.rename(columns=rename_columns, inplace=True)
+        
+    
+    # Eliminar columnas
+    if drop_columns:
+        cleaned_df.drop(columns=drop_columns, inplace=True)
+        
+        
+    # Categorizar columnas
+    if categorize_columns:
+        for column in categorize_columns:
+            if column in cleaned_df.columns:
+                cleaned_df[column] = cleaned_df[column].astype('category')
+            else:
+                print(f"La columna '{column}' no existe en el DataFrame.")
+                
+
+    # Reemplazar valores en columnas
+    if replace_values:
+        for column, replacements in replace_values.items():
+            cleaned_df[column].replace(replacements, inplace=True)
+            
+    # Agregar nuevas columnas
+    if new_columns:
+        for column, value in new_columns.items():
+            cleaned_df[column] = value
+            
+    # Agregar nuevas columnas basadas en otras columnas
+    if new_columns2:
+        for new_column, column_expr in new_columns2.items():
+            # Verificar si la expresión es proporcionada
+            if column_expr:
+                cleaned_df[new_column] = cleaned_df.eval(column_expr)
+            else:
+                cleaned_df[new_column] = None  # O cualquier valor predeterminado que prefieras
+                  
+    # Convertir columnas de fecha con formato específico
+    if convert_date_columns:
+        for column, date_format in convert_date_columns.items():
+            cleaned_df[column] = pd.to_datetime(cleaned_df[column], format=date_format, errors='coerce')
+
+    
+    # Convertir columnas a tipo de dato entero
+    if convert_to_int_columns:
+        for column in convert_to_int_columns:
+            cleaned_df[column] = pd.to_numeric(cleaned_df[column], errors='coerce').astype('Int64')
+    
+    
+    # Convertir columnas a tipo float
+    if convert_to_float:
+        for column in convert_to_float:
+            cleaned_df[column] = cleaned_df[column].astype(float)
+        
+            
+    return cleaned_df
---
+++ readme.md
@@ -0,0 +1,43 @@
+## Homicidios por siniestros viales en la Ciudad Autónoma de Buenos Aires, Argentina
+# Introducción
+
+![Imagen de accidentes de transito](https://www.prevencionintegral.com/sites/default/files/styles/ancho440px/public/blog/513858/field_foto/bloghbbenero2022.jpg)
+
+# Introducción 
+En este proyecto, se simula el rol de un Data Analyst que forma parte del equipo de analistas de datos de una empresa consultora. El Observatorio de Movilidad y Seguridad Vial (OMSV), bajo la órbita de la Secretaría de Transporte del Gobierno de la Ciudad Autónoma de Buenos Aires (CABA), solicitó la elaboración de un proyecto de análisis de datos.
+
+# Objetivo
+El objetivo principal es proporcionar información que permita a las autoridades locales tomar medidas para reducir la cantidad de víctimas fatales de los siniestros viales ocurridos en CABA. Se dispone de un dataset sobre homicidios en siniestros viales en la Ciudad de Buenos Aires durante el periodo 2016-2021.
+
+# Contexto
+Los siniestros viales son un problema importante en la Ciudad Autónoma de Buenos Aires debido al alto volumen de tráfico y la densidad poblacional. Estos incidentes pueden tener un impacto significativo en la seguridad de los residentes y visitantes de la ciudad, así como en la infraestructura vial y los servicios de emergencia.
+
+Actualmente, la población de CABA es de aproximadamente 3,120,612 habitantes en una superficie de 200 km², lo que implica una densidad de aproximadamente 15,603 habitantes por km². Además, en julio de 2023 se registraron 12,437,735 vehículos transitando por los peajes de las autopistas de acceso a CABA.
+
+# Datos
+Se utilizó una Base de `Homicidios` y `Lesiones` en formato Excel y , que contiene dos pestañas de datos: "HECHOS", que contiene información sobre los incidentes, y "VICTIMAS", que contiene información sobre las víctimas de esos incidentes. 
+
+# Tecnologías utilizadas
+Para este proyecto se emplearon Python y Pandas para el procesamiento de datos y el análisis exploratorio. También se realizó web scraping utilizando la librería BeautifulSoup para obtener datos complementarios. Para la visualización de datos, se utilizó Power BI.
+
+# ETL y EDA
+Se llevó a cabo un proceso de extracción, transformación y carga de los datos (ETL), seguido de un análisis exploratorio exhaustivo (EDA). Esto permitió identificar patrones que ayudarían a tomar medidas para reducir las víctimas de siniestros viales, se hizo la union vertical de las dos base de datos, `Homicidios` y `Lesiones`, de el cual se obtuvo un total de 28316 filas por 18 columnas, el cual se guardo como `siniestros_viales`. 
+
+# Análisis de los Datos
+Se analizó la variable temporal, el perfil de las víctimas, la distribución espacial de los incidentes, entre otros aspectos. Se encontró que el 70% de las víctimas fallecieron durante la semana, con un pico en diciembre. Además, el 77% de las víctimas son hombres, y el 42% de las víctimas viajaban en moto al momento del incidente.
+
+KPI
+Se plantearon tres objetivos relacionados con la disminución de las víctimas fatales de siniestros viales, junto con sus indicadores clave de rendimiento (KPI):
+
+* Reducir en un 10% la tasa de homicidios en siniestros viales de los últimos seis meses en CABA.
+* Reducir en un 7% la cantidad de accidentes mortales de motociclistas en el último año en CABA.
+`Propuesta de KPI`
+* Para este KPI se vio el analisis de datos, y obtuvimos que un 41.67% son accidentes en autopista, para esto se propuso e siguiente KPI:
+    Reducir en un 10% la cantidad de accidentes mortales en la comuna 1 en el último año, en CABA, respecto al año anterior.
+![Imagen de KPIS](https://github.com/katiaMOC/PI2_siniestros_viales_kat/imagenes/kpis.png)
+
+ 
+# Conclusiones y Recomendaciones
+Entre 2016 y 2021 se registraron 741 víctimas fatales en accidentes de tránsito en CABA. Se observó un alto porcentaje de víctimas masculinas para ser exactos 67.33% y una concentración de incidentes en la comuna 1, como tambien se pudo observar que la mayoria de accidentes fatales se dan en las autpistas. Se cumplió con el objetivo de reducir la tasa de homicidios en siniestros viales para el segundo semestre de 2021, pero no se lograron los objetivos de reducción de accidentes mortales de motociclistas ni de la tasa de homicidios en las autopistas para 2021.
+
+Se recomienda continuar monitoreando los objetivos propuestos y reforzar las campañas de seguridad vial, especialmente dirigidas a conductores de motos y usuarios de las avenidas, así como intensificar las campañas durante los fines de semana y en el mes de diciembre. Además, se sugiere dirigir las campañas de seguridad hacia los conductores debido a que son los que tienes finales fatales, tambien constituyen un grupo de alto riesgo.
